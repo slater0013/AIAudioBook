3c42cd0282eb7fee119f3ebc97267aaf8e5007e4
Guenael Sanchez <Guenael.Sanchez@gmail.com>
2026-07-17 23:06:05 +0200

    Initial Commit

---
+++ AIudioBook/AIudioBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIudioBookApp.swift
+//  AIudioBook
+//
+//  Created by sanchezg on 17/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct AIudioBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
